32ca2284f794cd8ffdcd99a02da1a7a2671d3680
BobLd <boris.ledeist@edhec.com>
2020-02-04 13:33:18 +0000

    Add Cohen-Sutherland and Liang-Barsky line clipping algorithms

---
+++ src/UglyToad.PdfPig/Geometry/Clipping/CohenSutherlandLineClipping.cs
@@ -0,0 +1,147 @@
+﻿using UglyToad.PdfPig.Core;
+
+namespace UglyToad.PdfPig.Geometry.Clipping
+{
+    /// <summary>
+    /// The Cohen–Sutherland algorithm is a computer-graphics algorithm used for line clipping.
+    /// </summary>
+    internal static class CohenSutherlandLineClipping
+    {
+        private const byte Inside = 0; // 0000
+        private const int Left = 1;    // 0001
+        private const int Right = 2;   // 0010
+        private const int Bottom = 4;  // 0100
+        private const int Top = 8;     // 1000
+
+        /// <summary>
+        /// Clip the line using the Cohen–Sutherland algorithm.
+        /// </summary>
+        internal static PdfLine? Clip(PdfLine line, PdfRectangle clippingRectangle)
+        {
+            var coords = Clip(
+                (double)line.Point1.X, (double)line.Point1.Y, (double)line.Point2.X, (double)line.Point2.Y,
+                (double)clippingRectangle.Left, (double)clippingRectangle.Right, (double)clippingRectangle.Bottom, (double)clippingRectangle.Top);
+            if (!coords.HasValue) return null;
+            return new PdfLine(coords.Value.point1, coords.Value.point2);
+        }
+
+        /// <summary>
+        /// Clip the line using the Cohen–Sutherland algorithm.
+        /// </summary>
+        internal static PdfPath.Line Clip(PdfPath.Line line, PdfRectangle clippingRectangle)
+        {
+            var coords = Clip(
+                (double)line.From.X, (double)line.From.Y, (double)line.To.X, (double)line.To.Y,
+                (double)clippingRectangle.Left, (double)clippingRectangle.Right, (double)clippingRectangle.Bottom, (double)clippingRectangle.Top);
+            if (!coords.HasValue) return null;
+            return new PdfPath.Line(coords.Value.point1, coords.Value.point2);
+        }
+
+        // Cohen–Sutherland clipping algorithm clips a line from
+        // P0 = (x0, y0) to P1 = (x1, y1) against a rectangle with 
+        // diagonal from (xmin, ymin) to (xmax, ymax).
+        private static (PdfPoint point1, PdfPoint point2)? Clip(double x0, double y0, double x1, double y1,
+            double xmin, double xmax, double ymin, double ymax)
+        {
+            // compute outcodes for P0, P1, and whatever point lies outside the clip rectangle
+            int outcode0 = ComputeOutCode(x0, y0, xmin, xmax, ymin, ymax);
+            int outcode1 = ComputeOutCode(x1, y1, xmin, xmax, ymin, ymax);
+            bool accept = false;
+
+            double x = 0;
+            double y = 0;
+
+            while (true)
+            {
+                if ((outcode0 | outcode1) == 0)
+                {
+                    // bitwise OR is 0: both points inside window; trivially accept and exit loop
+                    accept = true;
+                    break;
+                }
+                else if ((outcode0 & outcode1) != 0)
+                {
+                    // bitwise AND is not 0: both points share an outside zone (LEFT, RIGHT, TOP,
+                    // or BOTTOM), so both must be outside window; exit loop (accept is false)
+                    break;
+                }
+                else
+                {
+                    // failed both tests, so calculate the line segment to clip
+                    // from an outside point to an intersection with clip edge
+
+                    // At least one endpoint is outside the clip rectangle; pick it.
+                    int outcodeOut = outcode0 != Inside ? outcode0 : outcode1;
+
+                    // Now find the intersection point;
+                    // use formulas:
+                    //   slope = (y1 - y0) / (x1 - x0)
+                    //   x = x0 + (1 / slope) * (ym - y0), where ym is ymin or ymax
+                    //   y = y0 + slope * (xm - x0), where xm is xmin or xmax
+                    // No need to worry about divide-by-zero because, in each case, the
+                    // outcode bit being tested guarantees the denominator is non-zero
+                    if ((outcodeOut & Top) != 0)
+                    {
+                        // point is above the clip window
+                        x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0);
+                        y = ymax;
+                    }
+                    else if ((outcodeOut & Bottom) != 0)
+                    {
+                        // point is below the clip window
+                        x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0);
+                        y = ymin;
+                    }
+                    else if ((outcodeOut & Right) != 0)
+                    {
+                        // point is to the right of clip window
+                        y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0);
+                        x = xmax;
+                    }
+                    else if ((outcodeOut & Left) != 0)
+                    {
+                        // point is to the left of clip window
+                        y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0);
+                        x = xmin;
+                    }
+
+                    // Now we move outside point to intersection point to clip
+                    // and get ready for next pass.
+                    if (outcodeOut == outcode0)
+                    {
+                        x0 = x;
+                        y0 = y;
+                        outcode0 = ComputeOutCode(x0, y0, xmin, xmax, ymin, ymax);
+                    }
+                    else
+                    {
+                        x1 = x;
+                        y1 = y;
+                        outcode1 = ComputeOutCode(x1, y1, xmin, xmax, ymin, ymax);
+                    }
+                }
+            }
+
+            if (accept)
+            {
+                return (new PdfPoint((decimal)x0, (decimal)y0), new PdfPoint((decimal)x1, (decimal)y1));
+            }
+            return null;
+        }
+
+        private static int ComputeOutCode(double x, double y, double xmin, double xmax, double ymin, double ymax)
+        {
+            int code;
+
+            code = Inside;                      // initialised as being inside of [[clip window]]
+
+            if (x < xmin) code |= Left;         // to the left of clip window
+            else if (x > xmax) code |= Right;   // to the right of clip window
+
+            if (y < ymin) code |= Bottom;       // below the clip window
+            else if (y > ymax) code |= Top;     // above the clip window
+
+            return code;
+        }
+    }
+}
---
+++ src/UglyToad.PdfPig/Geometry/Clipping/LiangBarskyLineClipping.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using UglyToad.PdfPig.Core;
+
+namespace UglyToad.PdfPig.Geometry.Clipping
+{
+    /// <summary>
+    /// The Liang-Barsky algorithm is a computer-graphics algorithm used for line clipping.
+    /// </summary>
+    internal static class LiangBarskyLineClipping
+    {
+        /// <summary>
+        /// Clip the line using the Liang-Barsky algorithm.
+        /// </summary>
+        internal static PdfLine? Clip(PdfLine line, PdfRectangle clippingRectangle)
+        {
+            var coords = Clip(
+                (float)line.Point1.X, (float)line.Point1.Y, (float)line.Point2.X, (float)line.Point2.Y,
+                (float)clippingRectangle.Left, (float)clippingRectangle.Right, (float)clippingRectangle.Bottom, (float)clippingRectangle.Top);
+            if (!coords.HasValue) return null;
+            return new PdfLine(coords.Value.point1, coords.Value.point2);
+        }
+
+        /// <summary>
+        /// Clip the line using the Liang-Barsky algorithm.
+        /// </summary>
+        internal static PdfPath.Line Clip(PdfPath.Line line, PdfRectangle clippingRectangle)
+        {
+            var coords = Clip(
+                (float)line.From.X, (float)line.From.Y, (float)line.To.X, (float)line.To.Y,
+                (float)clippingRectangle.Left, (float)clippingRectangle.Right, (float)clippingRectangle.Bottom, (float)clippingRectangle.Top);
+            if (!coords.HasValue) return null;
+            return new PdfPath.Line(coords.Value.point1, coords.Value.point2);
+        }
+
+        private static (PdfPoint point1, PdfPoint point2)? Clip(float x1, float y1, float x2, float y2,
+            float xmin, float xmax, float ymin, float ymax)
+        {
+            float p1 = -(x2 - x1);
+            float p2 = -p1;
+            float p3 = -(y2 - y1);
+            float p4 = -p3;
+
+            float q1 = x1 - xmin;
+            float q2 = xmax - x1;
+            float q3 = y1 - ymin;
+            float q4 = ymax - y1;
+
+            if ((Math.Abs(p1) <= double.Epsilon && q1 < 0) ||
+                (Math.Abs(p3) <= double.Epsilon && q3 < 0) ||
+                (Math.Abs(p2) <= double.Epsilon && q2 < 0) ||
+                (Math.Abs(p4) <= double.Epsilon && q4 < 0))
+            {
+                return null; // Line is parallel and outside of clipping window
+            }
+
+            float[] posarr = new float[5];
+            float[] negarr = new float[5];
+            int posind = 1;
+            int negind = 1;
+            posarr[0] = 1;
+            negarr[0] = 0;
+
+            if (p1 != 0)
+            {
+                float r1 = q1 / p1;
+                float r2 = q2 / p2;
+                if (p1 < 0)
+                {
+                    negarr[negind++] = r1;
+                    posarr[posind++] = r2;
+                }
+                else
+                {
+                    negarr[negind++] = r2;
+                    posarr[posind++] = r1;
+                }
+            }
+
+            if (p3 != 0)
+            {
+                float r3 = q3 / p3;
+                float r4 = q4 / p4;
+                if (p3 < 0)
+                {
+                    negarr[negind++] = r3;
+                    posarr[posind++] = r4;
+                }
+                else
+                {
+                    negarr[negind++] = r4;
+                    posarr[posind++] = r3;
+                }
+            }
+
+            float xn1, yn1, xn2, yn2;
+            float rn1, rn2;
+            rn1 = Maxi(negarr, negind);
+            rn2 = Mini(posarr, posind);
+
+            if (rn1 > rn2)
+            {
+                return null;
+            }
+
+            xn1 = x1 + p2 * rn1;
+            yn1 = y1 + p4 * rn1;
+
+            xn2 = x1 + p2 * rn2;
+            yn2 = y1 + p4 * rn2;
+
+            return (new PdfPoint(Math.Round(xn1, 5), Math.Round(yn1, 5)),
+                    new PdfPoint(Math.Round(xn2, 5), Math.Round(yn2, 5)));
+        }
+
+        private static float Maxi(float[] arr, int n)
+        {
+            float m = 0;
+            for (int i = 0; i < n; ++i)
+                if (m < arr[i])
+                    m = arr[i];
+            return m;
+        }
+
+        private static float Mini(float[] arr, int n)
+        {
+            float m = 1;
+            for (int i = 0; i < n; ++i)
+                if (m > arr[i])
+                    m = arr[i];
+            return m;
+        }
+    }
+}
