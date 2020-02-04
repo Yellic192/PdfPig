49b37cd1bd417567e89ac271cb0e01750aee6f27
BobLd <boris.ledeist@edhec.com>
2020-02-04 13:26:45 +0000

    Remove useless using

--- src/UglyToad.PdfPig.DocumentLayoutAnalysis/TableExtractor.cs
+++ src/UglyToad.PdfPig.DocumentLayoutAnalysis/TableExtractor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using UglyToad.PdfPig.Content;
 using UglyToad.PdfPig.Core;
 using UglyToad.PdfPig.Geometry;
